34b1a1202b8e536db1b6324977fa1d2ba0c9d016
irfanmmr <irfanmaulanamr@student.upi.edu>
2023-01-03 09:26:00 +0700

    responsive rep

--- source/design.blade.php
+++ source/design.blade.php
@@ -3,8 +3,8 @@
 <div class="w-full flex flex-col">
     <div class="w-full h-screen flex flex-col">
         <div class="w-full my-auto flex-col flex items-center space-y-5">
-            <div class="flex flex-col space-y-3 lg:space-y-0 -mt-52 lg:-mt-48">
-                <p class="text-beatrice text-2xl lg:text-5xl leading-normal mx-auto w-8/12 text-center">
+            <div class="flex flex-col space-y-3 lg:space-y-5 -mt-52 lg:-mt-48">
+                <p class="text-beatrice text-2xl lg:text-5xl leading-relaxed mx-auto w-11/12 lg:w-8/12 text-center">
                     Impactful branding and high-end Webflow websites for creatives.
                 </p>
                 <p class="text-center mx-auto text-lg lg:text-xl">
@@ -12,7 +12,7 @@
                 </p>
             </div>
 
-            <div class="flex lg:flex-row px-5 lg:px-12 bottom-48 lg:bottom-6 absolute w-full justify-between">
+            <div class="flex lg:flex-row px-5 lg:px-12 bottom-32 lg:bottom-10 absolute w-full justify-between">
                 <div class="imagescontainer w-16 h-40 lg:w-52 lg:h-64  bg-white hover:scale-110 duration-300 transform shadow-lg container">
                     <img class="" src="../assets/1.png">
                 </div>
